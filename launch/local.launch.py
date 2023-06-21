c8ea7083ba2f90606c2f9e77909b5aa063b0f9f5
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-06-21 01:01:30 +0000

    Add hmi config

--- launch/local.launch.py
+++ launch/local.launch.py
@@ -9,6 +9,7 @@ from launchhelp import LaunchHelper
 def generate_launch_description():    
     launch_helper = LaunchHelper()
 
-    launch_helper.add_node_launch("phoenixpro_control_node", "phoenixpro_control_node.yaml")
+    # launch_helper.add_node_launch("phoenixpro_control_node", "phoenixpro_control_node.yaml")
+    launch_helper.add_node_launch("hmi_agent_ros2_sample_node", "hmi_agent_ros2_sample_node.yaml")
     
     return launch_helper.get_launch_description()
