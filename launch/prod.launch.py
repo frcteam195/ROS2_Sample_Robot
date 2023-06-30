17d858854a9b8a7c4e65541a5c0e2a45c0222ebb
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-06-30 23:59:02 +0000

    Update config connection string

--- launch/prod.launch.py
+++ launch/prod.launch.py
@@ -9,7 +9,7 @@ from launchhelp import LaunchHelper
 def generate_launch_description():    
     launch_helper = LaunchHelper()
 
-    launch_helper.add_node_launch("logger_ros2_node", "logger_ros2_node.yaml")
+    # launch_helper.add_node_launch("logger_ros2_node", "logger_ros2_node.yaml")
     launch_helper.add_node_launch("phoenixpro_control_node", "phoenixpro_control_node.yaml")
     launch_helper.add_node_launch("rio_control_ros2_node", "rio_control_ros2_node.yaml")
     
